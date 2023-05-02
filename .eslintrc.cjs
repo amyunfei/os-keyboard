61028030af48a5eabc732b7ad52dc4dc7fb8c566
郝云飞 <haoyf@botech.com.cn>
2023-05-02 20:57:27 +0800

    chore: eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,9 @@ module.exports = {
     'indent': ['error', 2, { 'ignoredNodes': ['VariableDeclaration[declarations.length=0]'] }],
     'semi': [2, 'never'],
     'quotes': [1, 'single'],
-    'no-trailing-spaces': ['error']
+    'no-trailing-spaces': ['error'],
+    'no-trailing-spaces': 'error',
+    'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 0, 'maxBOF': 0 }],
+    'space-infix-ops': ['error', {'int32Hint': false}],
   }
 }
